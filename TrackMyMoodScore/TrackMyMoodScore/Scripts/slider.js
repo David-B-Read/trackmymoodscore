2b7a02bc302935d4a62e5364ca45431c03b927a9
Dave Read <daveread@gmail.com>
2015-10-07 21:40:10 +0100

    Initial commit

---
+++ TrackMyMoodScore/TrackMyMoodScore/Scripts/slider.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $(".mood-score-slider").slider(
+        {
+            min: 0,
+            max: 10,
+            step: 1
+        });
+
+    $(".mood-score-slider").on("slide", updateMoodScore);
+    $(".mood-score-slider").click(updateMoodScore);
+
+});
+
+function updateMoodScore(moodScore) {
+    $(".current-mood-value").text(moodScore.value);
+}
