447482e8572cfbb4fec6e650bf15603c619db52e
Dave Read <daveread@gmail.com>
2015-10-08 19:13:37 +0100

    Slider implemented

--- TrackMyMoodScore/TrackMyMoodScore/Scripts/slider.js
+++ TrackMyMoodScore/TrackMyMoodScore/Scripts/slider.js
@@ -7,10 +7,20 @@
         });
 
     $(".mood-score-slider").on("slide", updateMoodScore);
-    $(".mood-score-slider").click(updateMoodScore);
+    $(".mood-score-slider").on("slideStop", updateMoodScore);
 
+    $(".save-my-score").on("click", saveMyScore);
+
+    updateMoodScore({ value: $(".mood-score-slider").slider("getValue") });
 });
 
 function updateMoodScore(moodScore) {
     $(".current-mood-value").text(moodScore.value);
 }
+
+function saveMyScore() {
+    var score = $(".mood-score-slider").slider("getValue");
+
+    alert(score);
+}
+
