e2e0751286f0c374e53b79aea506dbd7adfff48e
Dave Read <daveread@gmail.com>
2015-12-02 21:20:26 +0000

    Added graphing to site, reworked wording

--- TrackMyMoodScore/TrackMyMoodScore/Scripts/track-score.js
+++ TrackMyMoodScore/TrackMyMoodScore/Scripts/track-score.js
@@ -16,6 +16,8 @@
     getSavedState();
 
     updateMoodScore({ value: $(".mood-score-slider").slider("getValue") });
+
+    displayHistoricalScoresGraph();
 });
 
 function updateMoodScore(moodScore) {
@@ -46,3 +48,60 @@ function saveMyScore() {
     $.cookie("mood-score", savedMoodScores, { expires: 365 });
 }
 
+function displayHistoricalScoresGraph() {
+    if (savedMoodScores == null || savedMoodScores.length == 0)
+        return;
+
+    var dates = [];
+    for (var i = 0; i < savedMoodScores.length; i++) {
+        var date = new Date(savedMoodScores[i].date);
+        var xAxisDate = date.getDate() + '/'
+            + (date.getMonth() + 1) + '/'
+            + date.getFullYear();
+        dates.push(xAxisDate);
+    }
+    var scores = [];
+    for (var j = 0; j < savedMoodScores.length; j++) {
+        scores.push(savedMoodScores[j].score);
+    }
+
+    $('.graph-container').highcharts({
+        chart: {
+        type: 'column'
+    },
+        title: {
+        text: 'Your mood score history'
+    },
+        xAxis: {
+        categories: dates,
+        crosshair: false
+    },
+        yAxis: {
+        min: 0,
+        max: 10,
+        title: {
+        text: 'Mood Score'
+    }
+    },
+        tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+        plotOptions: {
+        column: {
+        pointPadding: 0.2,
+        borderWidth: 0
+    }
+    },
+        series: [{
+        name: 'Mood Score',
+        data: scores
+
+    }]
+    });
+	
+}
